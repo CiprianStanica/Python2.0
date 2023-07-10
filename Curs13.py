e4d1eae00de638af3d021e7fafc07104fe56b12f
Stanica Ionut Ciprian <stanicaciprian@yahoo.com>
2023-07-10 21:07:07 +0300

    Curs 13 incomplet

--- Curs13.py
+++ Curs13.py
@@ -6,5 +6,5 @@ def inmultire(primul_numar:int,al_doilea_numar = 14):
     :param int al doilea numar: al doilea numar care se inmulteste. Daca nu este specificata o valoare, numarul va fi automat 14
     '''
     return primul_numar * al_doilea_numar
-inmultire(2,3)
+print(inmultire(2,3))
 print(inmultire.__doc__)
